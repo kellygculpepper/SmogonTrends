b6523834f6e1415e26f1dbba9f77655d6dc00549
Kelly Culpepper <kelly.culpepper@duke.edu>
2023-06-23 22:12:29 -0400

    Update color script to process unique mons.

--- imgcolors.R
+++ imgcolors.R
@@ -41,41 +41,35 @@ add_IDs = function(df) {
   return(df)
 }
 
-get_URL = function(url, ID) {
-  # Parse the HTML from the URL
-  parsed_html = read_html(url)
+get_URL <- function(url, ID) {
   
-  # Extract the table rows
-  table_rows = parsed_html %>% html_nodes("tr")
+  parsed_html <- read_html(url)
   
-  # Loop over the table rows
+  table_rows <- parsed_html %>% html_nodes("tr")
+  
+  # loop over rows
   for (i in seq_along(table_rows)) {
-    row = table_rows[i]
+    row <- table_rows[i]
     
-    # Extract the ID cell
-    id_cells = row %>% html_nodes("td[style*='monospace']")
+    id_cells <- row %>% html_nodes("td[style*='monospace']")
     
-    # Check if the row contains an ID cell
+    # check if row has ID cell
     if (length(id_cells) == 0) {
       next
     }
     
-    id_cell = id_cells %>% html_text()
+    id_cell <- id_cells %>% html_text()
     
-    # Check if the ID matches the target ID
     if (id_cell == ID) {
-      # Extract the link
-      links = row %>% html_nodes("td > a") %>% html_attr("href")
+      links <- row %>% html_nodes("td > a") %>% html_attr("href")
       
-      # Check if any link was found
       if (length(links) > 0) {
-        # Return the first link only, prepended with base URL
+        # get first link
         return(paste0("https://bulbapedia.bulbagarden.net", links[1]))
       }
     }
   }
   
-  # Return NULL if no matching ID or URL was found
   return(NULL)
 }
 
@@ -94,40 +88,39 @@ get_img_URL = function(url) {
   return(img_url)
 }
 
-get_color = function(img_url) {
-  color = image_pal(file = img_url,
-                  n = 1, type = "div", k = 3, bw = c(0.1, 0.8), seq_by = "shv")
-  return(color)
-}
-
-add_colors = function(df) {
+add_colors <- function(df) {
   
   page = "https://bulbapedia.bulbagarden.net/wiki/List_of_Pokémon_by_National_Pokédex_number"
   
-  # Add IDs
-  df = add_IDs(df)
+  unique_pokemon <- df %>%
+    select(pokemon) %>%
+    distinct()
   
-  df = df %>%
+  unique_pokemon <- add_IDs(unique_pokemon)
+  
+  unique_pokemon <- unique_pokemon %>%
     mutate(URL = map_chr(ID, function(id) get_URL(page, id)))
   
-  df = df %>%
+  unique_pokemon <- unique_pokemon %>%
     mutate(img_URL = map_chr(URL, get_img_URL))
   
-  df = df %>%
+  unique_pokemon <- unique_pokemon %>%
     mutate(color = map_chr(img_URL, function(img_url) {
       image_pal(file = img_url,
                 n = 1, type = "div", k = 3, bw = c(0.1, 0.8), seq_by = "shv", plot = FALSE)
     }))
   
-  df = df %>%
+  df <- df %>%
+    left_join(unique_pokemon, by = "pokemon") %>%
     select(pokemon, ID, color)
   
   return(df)
 }
 
 
+
 # TEST CASE
-pokemon_names = c("Tauros", "Meowth-Alola")
+pokemon_names = c("Tauros", "Meowth-Alola", "Tauros")
 df = data.frame(pokemon = pokemon_names)
 final_df = df %>%
   add_colors()
