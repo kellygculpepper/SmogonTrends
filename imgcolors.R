cbb928fdf3a6ec95a8498a76b07ab2fc35cc8988
Kelly Culpepper <kelly.culpepper@duke.edu>
2023-06-25 00:29:56 -0400

    Added color data.

--- imgcolors.R
+++ imgcolors.R
@@ -7,7 +7,7 @@ library(purrr)
 mon_data = read.csv("data/Pokemon.csv", header = TRUE)
 
 getID = function(x) {
-  # nidoran handling, fix later (also need to fix urshifu single strike, possibly others)
+  # nidoran handling
   if (x == "NidoranF") return(29)
   if (x == "NidoranM") return(32)
   
@@ -88,35 +88,35 @@ get_img_URL = function(url) {
   return(img_url)
 }
 
-add_colors <- function(df) {
+add_colors <- function(data) {
   
   page = "https://bulbapedia.bulbagarden.net/wiki/List_of_Pokémon_by_National_Pokédex_number"
   
-  unique_pokemon <- df %>%
-    select(pokemon) %>%
-    distinct()
+  data$ID = sprintf("%04d", data$ID)
+  data$ID = paste0("#", data$ID)
   
-  unique_pokemon <- add_IDs(unique_pokemon)
-  
-  unique_pokemon <- unique_pokemon %>%
+  data <- data %>%
     mutate(URL = map_chr(ID, function(id) get_URL(page, id)))
   
-  unique_pokemon <- unique_pokemon %>%
+  data <- data %>%
     mutate(img_URL = map_chr(URL, get_img_URL))
   
-  unique_pokemon <- unique_pokemon %>%
+  data <- data %>%
     mutate(color = map_chr(img_URL, function(img_url) {
       image_pal(file = img_url,
                 n = 1, type = "div", k = 3, bw = c(0.1, 0.8), seq_by = "shv", plot = FALSE,
                 seed = 777)
     }))
   
-  df <- df %>%
-    left_join(unique_pokemon, by = "pokemon")
-  
-  return(df)
+  return(data)
 }
 
+
+mon_data = mon_data %>%
+  select(ID, Name, Form) %>%
+  add_colors()
+
+write.csv(mon_data, "data/colors.csv", row.names = FALSE)
 
 
 # TEST CASE
